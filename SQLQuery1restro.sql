fada973dae080faa35f1742fe1d52707fbbd206f
udnyanada <129100428+udnyanada@users.noreply.github.com>
2023-09-28 15:09:37 +0530

    Add files via upload

---
+++ SQLQuery1restro.sql
@@ -0,0 +1,12 @@
+use restro;
+
+create table item(
+id int identity(1,1) primary key,
+namei varchar(250) not null,
+category varchar(250) not null,
+price bigint not null);
+
+namei,category,price
+
+select * from item;
+
